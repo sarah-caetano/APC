a3c88b0c6d427454af0fbc2e3072104cf84b4b68
Sarah Caetano <45512659+sarah-caetano@users.noreply.github.com>
2020-05-28 17:16:04 -0300

    Add files via upload

---
+++ ArqTeste.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main(int argc, char **argv){
+
+	FILE *BOLAUM;
+	char string[10];
+	int lido;
+
+	BOLAUM=fopen("formosa.bin", "wb+");
+
+	if (BOLAUM==NULL)
+	{
+		printf("You can't go this way, man!\n");
+	}
+	fwrite(argv[1], sizeof(argv[1]), 1, BOLAUM);
+	fwrite(&argc, sizeof(argc), 1, BOLAUM);
+
+	fclose(BOLAUM);
+
+	BOLAUM=fopen("formosa.bin", "rb");
+
+	fread(string, sizeof(argv[1]), 1, BOLAUM);
+	fread(&lido, sizeof(lido), 1, BOLAUM);
+
+	printf("o argv lido foi: %s\n", string);
+	printf("o argc lido foi: %d\n", lido);
+
+	fclose(BOLAUM);
+
+
+	return 0;
+}
